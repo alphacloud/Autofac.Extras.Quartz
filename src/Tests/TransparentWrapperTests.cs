7aa07bb0dd1155ce03af341adb49012b4e3667a1
vk <vk@alphacloud.net>
2015-01-25 19:27:37 -0500

    Add unit-test to verify DisallowConcurrentExecution, closes #11

--- src/Tests/TransparentWrapperTests.cs
+++ src/Tests/TransparentWrapperTests.cs
@@ -3,7 +3,7 @@
 // Autofac Quartz integration
 // https://github.com/alphacloud/Autofac.Extras.Quartz
 // Licensed under MIT license.
-// Copyright (c) 2014 Alphacloud.Net
+// Copyright (c) 2014-2015 Alphacloud.Net
 
 #endregion
 
@@ -20,6 +20,7 @@ namespace Autofac.Extras.Quartz.Tests
     [TestFixture]
     public class TransparentWrapperTests
     {
+        private const string DisallowConcurrentExecution = "key.DisallowConcurrentExecution";
         private IContainer _container;
         private StdSchedulerFactory _factory;
         private IScheduler _scheduler;
@@ -29,6 +30,7 @@ namespace Autofac.Extras.Quartz.Tests
         {
             var cb = new ContainerBuilder();
             cb.RegisterType<SampleJob>();
+            cb.RegisterType<LongRunningJob>();
             _container = cb.Build();
 
             _factory = new StdSchedulerFactory();
@@ -40,6 +42,7 @@ namespace Autofac.Extras.Quartz.Tests
         [TearDown]
         public void TearDown()
         {
+            _scheduler.Shutdown(true);
             _container.Dispose();
         }
 
@@ -57,40 +60,66 @@ namespace Autofac.Extras.Quartz.Tests
             _scheduler.Start();
 
             Thread.Sleep(5.Seconds());
-            var jobMap = _scheduler.GetJobDetail(key)
-                .JobDataMap;
+
+            var jobMap = _scheduler.GetJobDetail(key).JobDataMap;
 
             jobMap.GetIntValue("counter").Should().Be(3);
         }
 
+        [Test]
+        public void ShouldRespectDisallowConcurrentExecutionAttribute()
+        {
+            var key = new JobKey("job2", "grp2");
+            var job1 = JobBuilder.Create<LongRunningJob>().WithIdentity(key).StoreDurably(true)
+                .Build();
+            var trigger =
+                TriggerBuilder.Create().WithSimpleSchedule(s => s.WithIntervalInSeconds(3).WithRepeatCount(10)).Build();
+
+            job1.ConcurrentExecutionDisallowed.Should().BeTrue("Concurrent execution should be disabled in JobDetail");
+
+            job1.JobDataMap["counter"] = 0;
+            job1.JobDataMap[DisallowConcurrentExecution] = false;
+
+            _scheduler.ScheduleJob(job1, trigger);
+            _scheduler.Start();
+
+            Thread.Sleep(7.Seconds());
+
+            var jobMap = _scheduler.GetJobDetail(key).JobDataMap;
+
+            jobMap.GetBooleanValue(DisallowConcurrentExecution)
+                .Should()
+                .BeTrue("Concurrent execution was indicated as enabled inside job execution");
+        }
+
         [UsedImplicitly]
         [PersistJobDataAfterExecution]
-        [DisallowConcurrentExecution]
         private class SampleJob : IJob
         {
             public void Execute(IJobExecutionContext context)
             {
                 var data = context.JobDetail.JobDataMap;
-                var counter = (int) data["counter"];
-                counter++;
-                Debug.WriteLine("counter: {0}", counter);
-                data["counter"] = counter;
+                var counter = data.GetIntValue("counter");
+                Debug.WriteLine("SampleJob - Counter: {0}, ConcurrentExecutionDisallowed: {1}", counter,
+                    context.JobDetail.ConcurrentExecutionDisallowed);
+                data["counter"] = counter + 1;
             }
         }
 
         [UsedImplicitly]
-        private class Wrapper : IJob
+        [PersistJobDataAfterExecution]
+        [DisallowConcurrentExecution]
+        private class LongRunningJob : IJob
         {
-            private readonly IJob _target;
-
-            public Wrapper(IJob target)
-            {
-                _target = target;
-            }
-
             public void Execute(IJobExecutionContext context)
             {
-                _target.Execute(context);
+                var data = context.JobDetail.JobDataMap;
+                var counter = data.GetIntValue("counter");
+                data[DisallowConcurrentExecution] = context.JobDetail.ConcurrentExecutionDisallowed;
+                Debug.WriteLine("Long Runinng Job - counter: {0}, ConcurrentExecutionDisallowed: {1}", counter,
+                    context.JobDetail.ConcurrentExecutionDisallowed);
+                data["counter"] = counter + 1;
+                Thread.Sleep(6.Seconds());
             }
         }
     }
